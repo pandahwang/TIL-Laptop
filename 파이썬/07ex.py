e78fda9107aebffd79d5699f7d029fac6696750a
pandahwang <pandahwang@naver.com>
2024-03-15 13:25:34 +0900

    함수 몇개

--- 파이썬/07ex.py
+++ 파이썬/07ex.py
@@ -1,13 +1,3 @@
-word = input('Enter any words')
-
-if word == 'banana':
-    print ('All right, bananas.')
-
-if word < 'banana':
-    print ('Your word,' + word + ', comes before banana.')
-elif word > 'banana':
-    print('Your word,' + word + ', comes after banana.')
-else:
-    print ('All right, bananas.')
-
-print(dir(word))
+a= input()
+a=float(a)
+print(format(a, .5f))
