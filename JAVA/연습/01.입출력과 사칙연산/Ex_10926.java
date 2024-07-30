b47fd5214a2db19f07b8b2bcb35aa633e69f39e1
pandahwang <pandahwang@naver.com>
2024-07-30 23:49:29 +0900

    fix: Baekjoon dir

--- JAVA/연습/01.입출력과 사칙연산/Ex_10926.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Ex_10926 {
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        System.out.println(a + "??!");
-    }
-}
